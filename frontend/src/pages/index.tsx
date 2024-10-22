dbede7932449930339990172b93fd543b140949d
niqzart <qwert45hi@yandex.ru>
2024-10-22 04:59:03 +0300

    feat: basic page layout (experiment)

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,8 +2,19 @@ import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 
 const IndexPage: React.FC<PageProps> = () => {
-  return <main>
-    <h1>Index</h1>
+  return <main className="w-screen h-screen content-center text-center">
+    <div className=" max-w-[600px] m-auto">
+      <h1 className="text-5xl font-extrabold tracking-tight text-primary">
+        Title
+      </h1>
+      <p className="leading-7 [&:not(:first-child)]:mt-4">
+        Line one. Line one. Line one. Line one. Line one. Line one. Line one. Line one. Line one. Line one. Line one. Line one. Line one. Line one. Line one. Line one. Line one. Line one. Line one. Line one. Line one.
+      </p>
+      <p className="leading-7 [&:not(:first-child)]:mt-4">
+        Line two. Line two. Line two. Line two. Line two. Line two. Line two. Line two. Line two. Line two. Line two. Line two. Line two. Line two. Line two. Line two. Line two. Line two. Line two. Line two. Line two. Line two. Line two. Line two. Line two. Line two.
+      </p>
+      <div className="text-lg font-semibold mt-4 text-primary">Question</div>
+    </div>
   </main>
 }
 
