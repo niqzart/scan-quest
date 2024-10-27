d0ee17376e81290557d6ab4154b2631a065d1ea9
niqzart <qwert45hi@yandex.ru>
2024-10-28 01:51:10 +0300

    feat: simple mocked signup form

--- frontend/src/layouts/public/code/signup.tsx
+++ frontend/src/layouts/public/code/signup.tsx
@@ -1,6 +1,18 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 import * as React from "react"
 
 const Signup: React.FC = () => {
+  const [username, setUsername] = React.useState("")
+
+  const onChangeUsername: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setUsername(event.target.value)
+  }
+
+  const register = () => {
+    console.log("Register!", username)
+  }
+
   return <>
     <h1 className="text-3xl font-extrabold tracking-tight text-primary">
       Welcome
@@ -9,8 +21,12 @@ const Signup: React.FC = () => {
       Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info Quest Info
     </p>
     <h2 className="mt-8 text-primary font-extrabold">
-      Registration Form Here
+      Let's go?
     </h2>
+    <div className="mt-2 flex w-full items-center space-x-2">
+      <Input type="text" placeholder="Username" value={username} onChange={onChangeUsername} />
+      <Button type="submit" onClick={register}>Join</Button>
+    </div>
   </>
 }
 
