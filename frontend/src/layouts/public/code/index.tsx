e6a74acb9075f45c2321b3cd352abaccc5b61ae6
niqzart <qwert45hi@yandex.ru>
2024-10-28 02:24:58 +0300

    feat: connect code finding to backend & add subpage switching logic

--- frontend/src/layouts/public/code/index.tsx
+++ frontend/src/layouts/public/code/index.tsx
@@ -1,23 +1,53 @@
+import fetcher from "@/api/fetcher"
 import * as React from "react"
-import Success from "./success"
+import InvalidCode from "./invalid-code"
 import Loading from "./loading"
 import Signup from "./signup"
+import Success from "./success"
 
 export type CodeLayoutProps = {
   code: string
 }
 
-type ViewModeT = "loading" | "signup" | "success"
+type ViewModeT = "loading" | "invalid-code" | "signup" | "success"
 
 const CodeLayout: React.FC<CodeLayoutProps> = ({ code }) => {
   const [viewMode, setViewMode] = React.useState<ViewModeT>("loading")
 
+  const onInvalidCode = () => setViewMode("invalid-code")
+  const onAuthRequired = () => setViewMode("signup")
+  const onSuccess = () => setViewMode("success")
+
+  React.useEffect(() => {
+    if (viewMode !== "loading") return
+    fetcher({
+      method: "POST",
+      path: "public/participants/me/found-goals",
+      query: { code },
+    }).then(({ response, json }) => {
+      if (response.ok) {
+        // TODO get data from newly found goal
+        onSuccess()
+      } else if (response.status === 401) {
+        onAuthRequired()
+      } else if (response.status === 404 && json.detail === "Goal not found") {
+        onInvalidCode()
+      } else if (response.status === 409 && json.detail === "Finding already exists") {
+        // TODO show a different message
+        onSuccess()
+      } else {
+        // TODO error handling
+        console.error(json)
+      }
+    }).catch(console.error)
+  })
+
   return <main className="w-screen h-screen content-center text-center p-2">
     <div className="max-w-[600px] m-auto">
       {viewMode === "loading" && <Loading />}
+      {viewMode === "invalid-code" && <InvalidCode />}
       {viewMode === "signup" && <Signup />}
       {viewMode === "success" && <Success />}
-      <p>{code}</p>
     </div>
   </main>
 }
---
+++ frontend/src/layouts/public/code/invalid-code.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+const InvalidCode: React.FC = () => {
+  return <>
+    <h1 className="text-3xl font-extrabold tracking-tight text-primary">
+      {"Invalid code"}
+    </h1>
+  </>
+}
+
+export default InvalidCode
