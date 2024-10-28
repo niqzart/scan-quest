89d331104c6d79abd14d124e0b6b62e9d21dd469
niqzart <qwert45hi@yandex.ru>
2024-10-28 04:18:02 +0300

    feat: add welcome page's title & message to quests

--- backend/app/models/quests_db.py
+++ backend/app/models/quests_db.py
@@ -3,7 +3,7 @@ from uuid import UUID, uuid4
 
 from annotated_types import MaxLen, MinLen
 from pydantic_marshals.sqlalchemy import MappedModel
-from sqlalchemy import String
+from sqlalchemy import String, Text
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.config import Base
@@ -15,7 +15,14 @@ class Quest(Base):
     id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
     name: Mapped[str] = mapped_column(String(100))
 
+    welcome_title: Mapped[str] = mapped_column(String(100))
+    welcome_message: Mapped[str] = mapped_column(Text)
+
     InputSchema = MappedModel.create(
-        columns=[(name, Annotated[str, MinLen(1), MaxLen(100)])]
+        columns=[
+            (name, Annotated[str, MinLen(1), MaxLen(100)]),
+            welcome_title,
+            welcome_message,
+        ]
     )
     ResponseSchema = InputSchema.extend(columns=[id])
