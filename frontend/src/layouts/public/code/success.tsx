c4ff10ac63ae07cccd2c9c6ee81caf725258093c
niqzart <qwert45hi@yandex.ru>
2024-10-28 04:42:50 +0300

    feat: displaying quest progress on success page

--- frontend/src/layouts/public/code/success.tsx
+++ frontend/src/layouts/public/code/success.tsx
@@ -1,4 +1,6 @@
-import { FindingT } from "@/api/types"
+import fetcher from "@/api/fetcher"
+import { FindingT, QuestProgressT } from "@/api/types"
+import LoadingSpinner from "@/components/ui/loader"
 import * as React from "react"
 
 export type SuccessProps = {
@@ -6,6 +8,22 @@ export type SuccessProps = {
 }
 
 const Success: React.FC<SuccessProps> = ({ finding }) => {
+  const [questProgress, setQuestProgress] = React.useState<QuestProgressT | null>(null)
+
+  React.useEffect(() => {
+    if (questProgress !== null) return
+    fetcher({
+      path: "public/participants/me/quest-progress"
+    }).then(({ response, json }) => {
+      if (response.ok) {
+        setQuestProgress(json as QuestProgressT)
+      } else {
+        // TODO error handling
+        console.error(json)
+      }
+    }).catch(console.error)
+  })
+
   return <>
     <h1 className="text-3xl font-extrabold tracking-tight text-primary mb-2">
       {finding.hint_title}
@@ -15,6 +33,11 @@ const Success: React.FC<SuccessProps> = ({ finding }) => {
         {item}
       </p>
     ))}
+    {
+      questProgress === null
+        ? <LoadingSpinner className="text-primary mx-auto mt-2" size={32} />
+        : <h2 className="text-xl font-bold text-primary">Found: {questProgress.found_goals}/{questProgress.total_goals}</h2>
+    }
   </>
 }
 
